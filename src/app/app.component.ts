28c6310553b015f1ad57256f9fcd73ad5045d434
Syed Umer Hasan <umerhasan2010@gmail.com>
2020-03-16 16:51:21 +0500

    Drag and drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  DragDropModule,
+  CdkDragDrop,
+  moveItemInArray
+} from "@angular/cdk/drag-drop";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'DragAndDrop';
+  title = "DragAndDrop";
+  // tslint:disable:max-line-length
+  movies = [
+    {
+      title: "Text Field",
+      field: "text"
+    },
+    {
+      title: "Number Field",
+      field: "number"
+    },
+    {
+      title: "Password Field",
+      field: "password"
+    },
+    {
+      title: "Text Area",
+      field: "textarea"
+    }
+  ];
+  // tslint:enable:max-line-length
+
+  drop(event: CdkDragDrop<{ title: string; poster: string }[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { DragDropModule } from "@angular/cdk/drag-drop";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, DragDropModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
