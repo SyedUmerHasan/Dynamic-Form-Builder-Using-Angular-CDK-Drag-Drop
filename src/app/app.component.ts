d0ded25e4b8e8e1196f5f68a69925e142cc13d63
Syed Umer Hasan <umerhasan2010@gmail.com>
2020-03-17 15:30:30 +0500

    Dynamic form working with opensource Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { FormControl } from "@angular/forms";
+import { FormGroup } from "@angular/forms";
 import { Component } from "@angular/core";
 import {
   DragDropModule,
@@ -15,25 +17,69 @@ export class AppComponent {
   title = "DragAndDrop";
   items = [
     {
-      name: "Text Area",
-      type: "textarea"
+      type: "text",
+      name: "firstName",
+      label: "First Name",
+      value: "",
+      required: true
     },
     {
-      name: "Text Field",
-      type: "text"
+      type: "text",
+      name: "lastName",
+      label: "Last Name",
+      value: "",
+      required: true
+    },
+    { type: "text", name: "email", label: "Email", value: "", required: true },
+    { type: "file", name: "picture", label: "Picture", required: true },
+    {
+      type: "dropdown",
+      name: "country",
+      label: "Country",
+      value: "in",
+      required: true,
+      options: [
+        { key: "in", label: "India" },
+        { key: "us", label: "USA" }
+      ]
     },
     {
-      name: "Password Field",
-      type: "password"
+      type: "radio",
+      name: "country",
+      label: "Country",
+      value: "in",
+      required: true,
+      options: [
+        { key: "m", label: "Male" },
+        { key: "f", label: "Female" }
+      ]
     },
     {
-      name: "Number Field",
-      type: "number"
+      type: "checkbox",
+      name: "hobby",
+      label: "Hobby",
+      required: true,
+      options: [
+        { key: "f", label: "Fishing" },
+        { key: "c", label: "Cooking" }
+      ]
     }
   ];
-
-  basket = [];
-
+  unsubcribe: any;
+  form: FormGroup;
+  field = [];
+  constructor() {
+    // this.field = this.items;
+    console.log("AppComponent -> constructor -> this.field", this.field);
+    this.form = new FormGroup({
+      fields: new FormControl(JSON.stringify(this.field))
+    });
+    this.unsubcribe = this.form.valueChanges.subscribe(update => {
+      // console.log(update);
+      // console.log("AppComponent -> constructor -> update", update);
+      this.field = JSON.parse(update.field);
+    });
+  }
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(
@@ -48,6 +94,25 @@ export class AppComponent {
         event.previousIndex,
         event.currentIndex
       );
+      console.log("AppComponent -> drop -> this.getFields()", this.getFields());
     }
+  }
+
+  onUpload(e) {
+    console.log(e);
+  }
+
+  getFields() {
+    return this.field;
+  }
+  getStatus() {
+    if (this.field.length !== 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  ngDestroy() {
+    this.unsubcribe();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
+import { DynamicFormBuilderModule } from "./dynamic-form-builder/dynamic-form-builder.module";
 import { DragDropModule } from "@angular/cdk/drag-drop";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, DragDropModule],
+  imports: [
+    BrowserModule,
+    DragDropModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DynamicFormBuilderModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
